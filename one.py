7fa68b894e2668817d5e065687ca2ab8e580a3a2
Satria DS <satria@aisensum.com>
2018-06-27 11:36:41 +0700

    First commit

---
+++ one.py
@@ -0,0 +1,23 @@
+# one.py
+
+def func():
+	print("FUNC() IN ONE.PY")
+
+print("TOP LEVEL IN ONE.PY")
+
+def function():
+	pass
+
+def function():
+	pass
+
+if __name__ == '__main__':
+	print('ONE.PY is being run directly!')
+else:
+	print('ONE.PY has been imported!')
+
+	# RUN THE SCRIPT 
+
+	function2()
+	function1()
+	
